e86519db98e8c8f7ce715c30394b08ac2fd57f0f
João Marcelo Raber Gomes <48732732+joaoo21@users.noreply.github.com>
2021-07-18 23:03:57 -0300

    Create exercicio3.php

---
+++ exercicio3.php
@@ -0,0 +1,30 @@
+  
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+  
+<?php
+$i = 1;
+$vet = array();
+$men = 10;
+$pos = 0;
+$exc = array();
+while ($i <= 5) {
+	$vet[] = mt_rand(0,10);
+	$i++;
+}
+$i=0;
+foreach($vet as $v){
+	if ($v < $men){
+	$men = $v;
+	$pos = $i;
+	} elseif ($v == $men){
+	$exc[] = $i;
+	}
+	$i++;
+}
+echo("Menor número é: ".$men . "<br>");
+echo("E está na posição: ".$pos);
+?>
